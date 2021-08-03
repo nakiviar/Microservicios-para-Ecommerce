638f29f238eb43db8fa9016027161944f89a48bb
nakiviar <54564415+nakiviar@users.noreply.github.com>
2021-08-03 03:01:38 -0500

    agregando Eureka server

---
+++ springboot-servicio-eureka-server/src/test/java/com/udemy/springboot/servicio/eureka/SpringbootServicioEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.springboot.servicio.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

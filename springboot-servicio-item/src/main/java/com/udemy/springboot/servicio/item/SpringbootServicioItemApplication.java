4293235a9a50e86b042a7fcbafced1f37c3efb7b
nakiviar <54564415+nakiviar@users.noreply.github.com>
2021-10-04 23:48:05 -0500

    Quitando anotaciones RIBBON
    Ribbon ya viene implicito en EUREKA

--- springboot-servicio-item/src/main/java/com/udemy/springboot/servicio/item/SpringbootServicioItemApplication.java
+++ springboot-servicio-item/src/main/java/com/udemy/springboot/servicio/item/SpringbootServicioItemApplication.java
@@ -3,11 +3,11 @@ package com.udemy.springboot.servicio.item;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+//import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableEurekaClient
-@RibbonClient(name="servicio-productos")
+//@RibbonClient(name="servicio-productos")
 @EnableFeignClients 
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
